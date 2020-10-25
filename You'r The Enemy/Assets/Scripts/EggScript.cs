c133c092999215afb2357575a5b33753b0d742a8
Dhruman Gupta <31363068+DaRealBerlm@users.noreply.github.com>
2020-10-26 01:10:50 +0530

    Simplified and made the script more efficient
    The script has been reduced, formatted and the if repetitiveness has been removed to make the script more efficient.

--- You'r The Enemy/Assets/Scripts/EggScript.cs
+++ You'r The Enemy/Assets/Scripts/EggScript.cs
@@ -7,48 +7,21 @@ public class EggScript : MonoBehaviour
     public float Speed;
     public Vector3 StartPosition;
 
-   
     private void Update()
     {
-            if (!Pspawnner.Stop)
-            {
-                Vector2 temp = transform.position;
-                if (Input.GetKey(KeyCode.D))
-                {
-                    temp.x += Speed;
-                }
-                if (Input.GetKey(KeyCode.A))
-                {
-                    temp.x -= Speed;
-                }
-                if (Input.GetKey(KeyCode.W))
-                {
-                    temp.y += Speed;
-                }
-                if (Input.GetKey(KeyCode.S))
-                {
-                    temp.y -= Speed;
-                }
-                if (temp.x > 7.7f)
-                {
-                    temp.x = 7.7f;
-                }
-                if (temp.x < -7.7f)
-                {
-                    temp.x = -7.7f;
-                }
-                if (temp.y > 4f)
-                {
-                    temp.y = 4f;
-                }
-                if (temp.y < -4f)
-                {
-                    temp.y = -4f;
-                }
-                transform.position = temp;
-            }
-       
+        if (!Pspawnner.Stop)
+        {
+            float vertical = Input.GetAxis("Vertical")
+            float horizontal = Input.GetAxis("Horizontal")
+            Vector2 temp = transform.position;
+                
+            temp.x += Speed * horizontal;
+            temp.y += Speed * vertical;
+            
+            temp.x = Mathf.Clamp(temp.x, -7.7f, 7.7f);
+            temp.y = Mathf.Clamp(temp.y, -4f, 4f);
+
+            transform.position = temp;
+        }
     }
- 
-  
-    }
+}
